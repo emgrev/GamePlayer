21f3ec251ee04eedf18bc77d2c765d40ed798107
Rob <rlwest@gmail.com>
2019-10-10 14:40:26 -0400

    create a python 3 version

---
+++ Python3version/Manager.py
@@ -0,0 +1,11 @@
+import sys
+import ccm
+from ccm.lib.actr import *
+from random import randrange, uniform
+
+#######################
+##### The Manager #####
+#######################
+
+class Manager(ACTR):
+    pass
