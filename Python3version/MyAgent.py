14584a21aa5a6131e5c4008ee4daabfa8de50b43
Rob <rlwest@gmail.com>
2019-10-18 13:55:49 -0400

    removed un needed code
    removed extra code not needed to end a planning unit

--- Python3version/MyAgent.py
+++ Python3version/MyAgent.py
@@ -115,7 +115,7 @@ class MyAgent(ACTR):
                                b_unit_task='unit_task:?unit_task state:finished type:ordered'):
         DM.request('planning_unit:?planning_unit cue:?unit_task unit_task:? cuelag:?cue')
         b_plan_unit.modify(state='retrieve')
-        print (' finished unit task = '), unit_task
+        print (' finished unit task = '), ?unit_task
 
     def retrieve_next_unit_task(b_plan_unit='state:retrieve',
                                 b_DM='planning_unit:?planning_unit cuelag:?cuelag cue:?cue!finished unit_task:?unit_task'):
@@ -126,27 +126,24 @@ class MyAgent(ACTR):
 
 ########################## these manage planning units that are finished ###################
 
-    def last_unit_task_ordered_plan(b_context='have_plan:yes',
-                                    b_plan_unit='planning_unit:?planning_unit',
-                                    b_unit_task='unit_task:finished state:start type:ordered',
-                                    utility=5):  # high priority if a plan was generated in the planning unit
+    def last_unit_task_ordered_plan(b_plan_unit='planning_unit:?planning_unit',
+                                    b_unit_task='unit_task:finished state:start type:ordered'):
         print ('finished planning unit='),planning_unit
         b_unit_task.set('stop')
-        b_context.modify(status='unoccupied', have_plan='no') # have plan always needs to be re-set to no
+        b_context.modify(status='unoccupied') # have plan always needs to be re-set to no
 
-    def last_unit_task_ordered_noplan(b_plan_unit='planning_unit:?planning_unit',
-                                      b_unit_task='unit_task:finished state:start type:ordered',
-                                      utility=1):  # by default no plan is generated
-        print ('finished planning unit='),planning_unit
-        b_unit_task.set('stop')
-        #b_plan_unit.modify(planning_unit='none')
-#        b_context.modify(planning_unit='none', have_plan='no') # have plan always needs to be re-set to no
+##    def last_unit_task_ordered_noplan(b_plan_unit='planning_unit:?planning_unit',
+##                                      b_unit_task='unit_task:finished state:start type:ordered'):
+##        print ('finished planning unit='),planning_unit
+##        b_unit_task.set('stop')
+##        #b_plan_unit.modify(planning_unit='none')
+###        b_context.modify(planning_unit='none', have_plan='no') # have plan always needs to be re-set to no
 
 #################### Stop Program ######################
 
-    def stop_production(b_unit_task='stop'):
-        print ('Task complete. Good bye!')
-        self.stop()
+##    def stop_production(b_unit_task='stop'):
+##        print ('Task complete. Good bye!')
+##        self.stop()
 
 #################################
 ##### Unit Task Productions #####
