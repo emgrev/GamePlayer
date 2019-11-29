aa0fd013eec07617c87998d8dc4b01c2586a0346
Rob <rlwest@gmail.com>
2019-11-28 20:37:55 -0500

    random planning units
    works

--- Python3version/MyAgent.py
+++ Python3version/MyAgent.py
@@ -65,51 +65,34 @@ class MyAgent(ACTR):
     ## they can take any ACT-R form (one production or more) but must eventually call a planning unit and update the context buffer
 
 
-    def START_start(focus='start', b_context='status:unoccupied planning_unit:none'):
+    def START_start(b_context='status:unoccupied planning_unit:none'):
         b_method.set('method:get_code target:response content:0000 state:start')
-        focus.set('get_code')
         b_unit_task.set('unit_task:START state:running')
+        b_context.modify(status='starting_game')
         print ('waiting to see code')
 
-    def START_AK(b_unit_task='unit_task:START state:running',
+    def START_AK(b_context='status:starting_game planning_unit:none',
+                 b_unit_task='unit_task:START state:running',
                  b_method='state:finished', b_visual='AK'):  ###!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         b_plan_unit.modify(planning_unit='AK',cuelag='none',cue='start',unit_task='AK',state='begin_sequence',ptype='ordered')
         b_context.modify(status='occupied')
         print ('run_AK_PU')
 
-    def START_RP(b_unit_task='unit_task:START state:running',
+    def START_RP(b_context='status:starting_game planning_unit:none',
+                 b_unit_task='unit_task:START state:running',
                  b_method='state:finished', b_visual='RP'):  ###!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        b_plan_unit.modify(planning_unit='RP',cuelag='none',cue='start',unit_task='AK',state='begin_sequence',ptype='ordered')
+        b_plan_unit.modify(planning_unit='RP',cuelag='none',cue='start',unit_task='RP',state='begin_sequence',ptype='ordered')
         b_context.modify(status='occupied')
         print ('run_RP_PU')
 
-    def START_HW(b_unit_task='unit_task:START state:running',
+    def START_HW(b_context='status:starting_game planning_unit:none',
+                 b_unit_task='unit_task:START state:running',
                  b_method='state:finished', b_visual='HW'):  ###!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        b_plan_unit.modify(planning_unit='HW',cuelag='none',cue='start',unit_task='AK',state='begin_sequence',ptype='ordered')
+        b_plan_unit.modify(planning_unit='HW',cuelag='none',cue='start',unit_task='HW',state='begin_sequence',ptype='ordered')
         b_context.modify(status='occupied')
         print ('run_HW_PU')
 
 
-        
-##    def run_AK_PU(b_context='status:seen planning_unit:none',
-##                  b_visual='AK'):
-##        b_plan_unit.modify(planning_unit='AK',cuelag='none',cue='start',unit_task='AK',state='begin_sequence',ptype='ordered')
-##        b_context.modify(status='occupied')
-##        print ('run_AK_PU')
-##
-##    def run_RP_PU(b_context='status:seen planning_unit:none',
-##                  b_visual='RP'):
-##        #b_context='status:unoccupied planning_unit:RP'):
-##        b_plan_unit.modify(planning_unit='RP',cuelag='none',cue='start',unit_task='RP',state='begin_sequence',ptype='ordered')
-##        b_context.modify(status='occupied')
-##        print ('run_RP_PU')
-##
-##    def run_HW_PU(b_context='status:seen planning_unit:none',
-##                  b_visual='HW'):
-##        #b_context='status:unoccupied planning_unit:HW'):
-##        b_plan_unit.modify(planning_unit='HW',cuelag='none',cue='start',unit_task='HW',state='begin_sequence',ptype='ordered')
-##        b_context.modify(status='occupied')
-##        print ('run_HW_PU')
 
 #######################################################
 ########## unit task management productions ###########
@@ -146,45 +129,12 @@ class MyAgent(ACTR):
         ############################### referee
         choices = ['AK','RP','HW']
         x=random.choice(choices)
-        print ('next code is')
+        print ('next code is &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
         print (x)
         motor.referee_action('display', 'state', x)
         ################################
 
 
-
-#################
-##### START Planning Unit - used to start the game
-#################
-
-#   AK
-# < RP
-#   HW                   
-
-##    def START_ordered(b_unit_task='unit_task:START state:start type:ordered'): ### this unit task is chosen to fire by planning unit
-##        b_unit_task.modify(state='begin')
-##        print ('start unit task START')
-##
-##    ## the first production in the unit task must begin this way
-##    def START_start(b_unit_task='unit_task:START state:begin type:?type'):
-##        b_method.set('method:get_code target:response content:0000 state:start')
-##        focus.set('get_code')
-##        b_unit_task.set('unit_task:START state:running type:?type')
-##        print ('waiting to see code')
-##
-##    def START_AK(b_unit_task='unit_task:START state:running type:?type',
-##                 b_method='state:finished', b_visual='?ak'):  ###!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-##        b_plan_unit.modify(planning_unit='AK',cuelag='none',cue='start',unit_task='AK',state='begin_sequence',ptype='ordered')
-##        print (ak)
-##        b_context.modify(status='occupied')
-##        print ('run_AK_PU')
-##        b_unit_task.set('unit_task:START state:finished type:ordered')
-
-##        motor.vision_finst_reset()
-##        b_method.modify(state='finished')
-##        focus.set('code:identified')
-##        print ('I have seen the code, it is')
-##        print (self.b_visual)
 
 #################
 ##### AK UT #####
