1e52131e33c24321172496e023bf4741f1a96f44
EliReid <16.elisabethw@gmail.com>
2020-02-22 17:55:14 -0500

    updating final RTModule class. File recording altered for deep copy compatability

--- Python3version/RTModule.py
+++ Python3version/RTModule.py
@@ -1,7 +1,6 @@
-import time
 import ccm
+import time
 import os
-from ccm.lib.actr import *
 from datetime import datetime
 
 ### TODO: Need a way to add additional arguments so new features/columns can be easily added
@@ -9,7 +8,7 @@ from datetime import datetime
 
 class RTModule(ccm.Model):
     #clock = time.time()
-    last_time = None
+    last_time = time.time()
 
     def __init__(self, file_name=None):
 
@@ -19,22 +18,23 @@ class RTModule(ccm.Model):
         # Set up header for .csv file format
         if not os.path.exists(self._file_name):
             print("Writing new file")
-            self._file_handle = open(self._file_name, "w")
-            self._file_handle.write("agent,time_stamp,rt(ns)\n")
-        else:
-            self._file_handle = open(self._file_name, "a")
+            open(self._file_name, "w").write("agent,time_stamp,rt(seconds)\n")
 
-        self.last_time = time.time_ns()
+        self.last_time = time.time()
 
-    def recordRT(self):
-        rt = (time.time_ns() - self.last_time) # Divide by 1000000 to convert into seconds
+    def recordRT(self, *args):
+        rt = (time.time() - self.last_time)
 
         col = "agent," + '"' + str(datetime.fromtimestamp(time.time())) + '"' + "," + str(rt)
-        print(col)
 
-        self.last_time = time.time_ns()
-        self._file_handle.write(col + "\n")
+        for x in args:
+            col = col + "," + '"' + str(x) + '"'
 
-test = RTModule()
-test.recordRT()
-test.recordRT()
+        self.last_time = time.time()
+        f = open(self._file_name, "a")
+        f.write(col + "\n")
+        f.close()
+
+#test = RTModule()
+#test.recordRT()
+#test.recordRT()
