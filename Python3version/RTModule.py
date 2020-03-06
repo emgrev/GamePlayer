ac1f36c4422677d65c864ee06ebedabf43f293c9
EliReid <16.elisabethw@gmail.com>
2020-03-06 15:47:04 -0500

    Fix for file writing when running with IDLE

--- Python3version/RTModule.py
+++ Python3version/RTModule.py
@@ -11,12 +11,14 @@ class RTModule(ccm.Model):
 
         if file_name == None:
             self._file_name = os.path.join(os.curdir, "rtData.csv")
+        else:
+            self._file_name = file_name
 
         # Set up header for .csv file format
         if not os.path.exists(self._file_name):
             print("Writing new file")
             self.fh = ShallowFileHandle(self._file_name)
-            self.fh.file_handle.write("agent,time_stamp,rt(seconds)\n")
+            self.fh.write("agent,time_stamp,rt(seconds)\n")
 
         else:
             self.fh = ShallowFileHandle(self._file_name)
@@ -26,20 +28,28 @@ class RTModule(ccm.Model):
     def recordRT(self, *args):
         rt = (time.time() - self.last_time)
 
-        col = "agent," + '"' + str(datetime.fromtimestamp(time.time())) + '"' + "," + str(rt)
-
+        # Create the row entry string with rt time and extra args
+        entry = "agent," + '"' + str(datetime.fromtimestamp(time.time())) + '"' + "," + str(rt)
         for x in args:
-            col = col + "," + '"' + str(x) + '"'
+            entry = entry + "," + '"' + str(x) + '"'
 
         self.last_time = time.time()
-        self.fh.file_handle.write(col + "\n")
+
+        # TODO: Check the return value when file is appended to
+        self.fh.write(entry + "\n")
 
 
 class ShallowFileHandle():
 
     def __init__(self, file_name):
-            self._file_name = os.path.join(os.curdir,file_name)
-            self.file_handle = open(self._file_name, "a")
+        self._file_name = os.path.join(os.curdir,file_name)
+
+        # TODO: Check the return value when file is opened
+        self.file_handle = open(self._file_name, "a")
+
+    def write(self, x):
+        self.file_handle.write(x)
+        self.file_handle.flush()
 
     def __deepcopy__(self, memo):
         return self
